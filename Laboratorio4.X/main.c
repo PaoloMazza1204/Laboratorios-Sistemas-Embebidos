07d52eed046d5dc6d01ad851b4019f25c5b9b82b
sebaColina <sebacoli00@gmail.com>
2021-05-31 22:42:52 -0300

    re crudo

---
+++ Laboratorio4.X/main.c
@@ -0,0 +1,132 @@
+/******************************************************************************
+ * ANALISIS ARBOL DEL PROYECTO (PARTE 1 B)                                    *
+ *                                                                            *
+ *                                                                            *
+ *                                                                            *
+ ******************************************************************************/
+
+/**
+  Section: Included Files
+ */
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "mcc_generated_files/system.h"
+#include "mcc_generated_files/pin_manager.h"
+
+void blinkLED(void *p_param);
+
+/*
+                         Main application
+ */
+int main(void) {
+    // initialize the device
+    SYSTEM_Initialize();
+
+    /* Create the tasks defined within this file. */
+    xTaskCreate(blinkLED, "task1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+
+    /* Finally start the scheduler. */
+    vTaskStartScheduler();
+
+    /* If all is well, the scheduler will now be running, and the following line
+    will never be reached.  If the following line does execute, then there was
+    insufficient FreeRTOS heap memory available for the idle and/or timer tasks
+    to be created.  See the memory management section on the FreeRTOS web site
+    for more details. */
+    for (;;);
+}
+
+void blinkLED(void *p_param) {
+    for (;;) {
+        LEDA_SetLow();
+        vTaskDelay(pdMS_TO_TICKS(800));
+        LEDA_SetHigh();
+        vTaskDelay(pdMS_TO_TICKS(400));
+    }
+}
+
+void userInterface(void *) {
+
+}
+
+void vApplicationMallocFailedHook(void) {
+    /* vApplicationMallocFailedHook() will only be called if
+    configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+    function that will get called if a call to pvPortMalloc() fails.
+    pvPortMalloc() is called internally by the kernel whenever a task, queue,
+    timer or semaphore is created.  It is also called by various parts of the
+    demo application.  If heap_1.c or heap_2.c are used, then the size of the
+    heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+    FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+    to query the size of free heap space that remains (although it does not
+    provide information on how the remaining heap might be fragmented). */
+    taskDISABLE_INTERRUPTS();
+    for (;;);
+}
+
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook(void) {
+    /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+    to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+    task.  It is essential that code added to this hook function never attempts
+    to block in any way (for example, call xQueueReceive() with a block time
+    specified, or call vTaskDelay()).  If the application makes use of the
+    vTaskDelete() API function (as this demo application does) then it is also
+    important that vApplicationIdleHook() is permitted to return to its calling
+    function, because it is the responsibility of the idle task to clean up
+    memory allocated by the kernel to any task that has since been deleted. */
+}
+
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName) {
+    (void) pcTaskName;
+    (void) pxTask;
+
+    /* Run time task stack overflow checking is performed if
+    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook	function is 
+    called if a task stack overflow is detected.  Note the system/interrupt
+    stack is not checked. */
+    taskDISABLE_INTERRUPTS();
+    for (;;);
+}
+
+/*-----------------------------------------------------------*/
+
+void vApplicationTickHook(void) {
+    /* This function will be called by each tick interrupt if
+    configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
+    added here, but the tick hook is called from an interrupt context, so
+    code must not attempt to block, and only the interrupt safe FreeRTOS API
+    functions can be used (those that end in FromISR()). */
+}
+
+/*-----------------------------------------------------------*/
+
+/*-----------------------------------------------------------*/
+
+void vAssertCalled(const char * pcFile, unsigned long ulLine) {
+    volatile unsigned long ul = 0;
+
+    (void) pcFile;
+    (void) ulLine;
+
+    __asm volatile( "di");
+    {
+        /* Set ul to a non-zero value using the debugger to step out of this
+        function. */
+        while (ul == 0) {
+            portNOP();
+        }
+    }
+    __asm volatile( "ei");
+}
+
+/**
+ End of File
+ */
+
